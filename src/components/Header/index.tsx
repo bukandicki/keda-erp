bbcc17be6ec19bd6bf6572c2937ec60c05ed8610
Dicki Maulana <bukandicki@gmail.com>
2024-08-07 21:58:59 +0700

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+export default function HeaderComponent() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+
+      <nav className="header__nav">
+        <ul className="nav__lists">
+          <li className="nav__item">
+
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
