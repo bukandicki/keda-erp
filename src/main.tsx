f9dac002bc0cb4fce5cdcb0ec8e807a7d561ae49
Dicki Maulana <bukandicki@gmail.com>
2024-08-07 22:00:47 +0700

    refactor(main): update main code

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-  </React.StrictMode>,
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import ReactLenis from 'lenis/react'
+
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import RouterProvider from "./components/RouterProvider"
+
+import "./index.styles.scss"
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ReactLenis root>
+      <Header />
+      <RouterProvider />
+      <Footer />
+    </ReactLenis>
+  </StrictMode>,
 )
