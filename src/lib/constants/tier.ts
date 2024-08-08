3d010b566826a9a6e2003eabc3badbf826d84de8
Dicki Maulana <bukandicki@gmail.com>
2024-08-09 01:04:13 +0700

    chore(lib): add tier list constant

---
+++ src/lib/constants/tier.ts
@@ -0,0 +1,61 @@
+export const TIER_LISTS: Array<{
+  name: string;
+  description: string;
+  price: number;
+  popular: boolean;
+  benefits: {
+    can_record_incoming_items: boolean;
+    can_record_outgoing_items: boolean;
+    can_record_profits: boolean;
+    can_analyze_sales_with_charts: boolean;
+    has_24_7_support: boolean;
+    can_export_data_to_excel: boolean;
+    can_use_ai_prediction: boolean;
+  }
+}> = [
+  {
+    name: "Basic",
+    description: "Ideal for small businesses or startups that need essential tools to manage incoming and outgoing items, as well as track profits.",
+    price: 2300,
+    popular: false,
+    benefits: {
+      can_record_incoming_items: true,
+      can_record_outgoing_items: true,
+      can_record_profits: true,
+      can_analyze_sales_with_charts: false,
+      has_24_7_support: false,
+      can_export_data_to_excel: false,
+      can_use_ai_prediction: false,
+    }
+  },
+  {
+    name: "Business",
+    description: "Perfect for growing businesses that require more advanced features like sales analysis and 24/7 support to optimize their operations.",
+    price: 5800,
+    popular: true,
+    benefits: {
+      can_record_incoming_items: true,
+      can_record_outgoing_items: true,
+      can_record_profits: true,
+      can_analyze_sales_with_charts: true,
+      has_24_7_support: true,
+      can_export_data_to_excel: false,
+      can_use_ai_prediction: false,
+    }
+  },
+  {
+    name: "Entrepreneur",
+    description: "Designed for established businesses or entrepreneurs seeking comprehensive tools including advanced sales analysis, data export, AI-driven predictions, and round-the-clock support.",
+    price: 8000,
+    popular: false,
+    benefits: {
+      can_record_incoming_items: true,
+      can_record_outgoing_items: true,
+      can_record_profits: true,
+      can_analyze_sales_with_charts: true,
+      has_24_7_support: true,
+      can_export_data_to_excel: true,
+      can_use_ai_prediction: true,
+    }
+  }
+];
